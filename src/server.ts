865ce6474398ddde86d769bd2d3614482e5fd0f7
klcantrell <cantrellkalalau@gmail.com>
2018-12-14 20:50:58 -0500

    add basic graphql for all characters query

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,42 @@ import express from 'express';
 import awsServerlessExpress from 'aws-serverless-express';
 import { DataMapper } from '@aws/dynamodb-data-mapper';
 import { attribute, hashKey, table } from '@aws/dynamodb-data-mapper-annotations';
+import { ApolloServer, gql } from 'apollo-server-express'
 
 import dynamodb from './dynamodb';
 
 const app = express();
 const mapper = new DataMapper({ client: dynamodb });
+
+const typeDefs = gql`
+  type Query {
+    characters: [Character]!
+  }
+
+  type Character {
+    id: Int
+    name: String
+    game: String
+    hometown: String
+    weapon: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    characters: async () => {
+      let data = [];
+      const iterator = mapper.scan(CharacterModel);
+      for await (const record of iterator) {
+        data.push(record);
+      }
+      return data;
+    }
+  }
+}
+
+const gqlServer = new ApolloServer({ typeDefs, resolvers });
+gqlServer.applyMiddleware({ app });
 
 @table('ff_characters')
 class CharacterModel {
@@ -24,11 +55,7 @@ class CharacterModel {
 
   @attribute()
   weapon?: string;
-
-  @attribute()
-  type?: string;
 }
-
 
 app.get('/characters', async (req, res) => {
   let data = [];
