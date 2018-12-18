2f8c51a2bf0b88033842afd042c4f98f1972a38b
klcantrell <cantrellkalalau@gmail.com>
2018-12-17 21:51:37 -0500

    query for game returns GameUnion type * format with Prettier

--- src/gqlServer.ts
+++ src/gqlServer.ts
@@ -1,13 +1,28 @@
-import { ApolloServer, gql } from 'apollo-server-express'
-import { getAllCharacters, getCharacter, getGame, getAllGames } from './dynamodb';
+import { ApolloServer, gql } from 'apollo-server-express';
+import {
+  getAllCharacters,
+  getCharacter,
+  getGame,
+  getAllGames,
+} from './dynamodb';
 
 interface IQueryArgs {
   id: number;
 }
 
-interface IParent {}
+interface IUnusedParams {}
+
+interface IInfoParam {
+  fieldNodes: Array<{ selectionSet: { selections: any } }>;
+}
+
+interface IObjParam {
+  release_date: string;
+}
 
 const typeDefs = gql`
+  union GameUnion = GameInfo | Game
+
   type Query {
     characters(id: Int): [Character]!
     games(id: Int): [Game]!
@@ -16,9 +31,14 @@ const typeDefs = gql`
   type Character {
     id: Int
     name: String
-    game: String
+    game: GameUnion
     hometown: String
     weapon: String
+  }
+
+  type GameInfo {
+    id: Int
+    name: String
   }
 
   type Game {
@@ -29,14 +49,61 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
-  Query: {
-    characters: (_: IParent, { id }: IQueryArgs) => {
-      return id ? getCharacter(id) : getAllCharacters();
+  GameUnion: {
+    __resolveType(obj: IObjParam) {
+      if (obj.release_date) {
+        return 'Game';
+      }
+      return 'GameInfo';
     },
-    games: (_: IParent, { id }: IQueryArgs) => {
+  },
+  Query: {
+    async characters(
+      _: IUnusedParams,
+      { id }: IQueryArgs,
+      __: IUnusedParams,
+      info: IInfoParam
+    ) {
+      const characterData = id
+        ? await getCharacter(id)
+        : await getAllCharacters();
+      const queryGameSelection = info.fieldNodes[0].selectionSet.selections.find(
+        (selection: any) => {
+          return selection.name && selection.name.value === 'game';
+        }
+      );
+      if (queryGameSelection) {
+        const nestedGameQuery = queryGameSelection.selectionSet.selections.find(
+          (selection: any) => {
+            return (
+              selection.typeCondition &&
+              selection.typeCondition.name &&
+              selection.typeCondition.name.value === 'Game'
+            );
+          }
+        );
+        if (nestedGameQuery) {
+          const characterWithGameData = characterData.map(async character => {
+            const [gameData] = await getGame(character.game.id);
+            return {
+              ...character,
+              game: gameData,
+            };
+          });
+          return characterWithGameData;
+        }
+      }
+      return characterData;
+    },
+    async games(
+      _: IUnusedParams,
+      { id }: IQueryArgs,
+      __: IUnusedParams,
+      info: IInfoParam
+    ) {
       return id ? getGame(id) : getAllGames();
-    }
-  }
-}
+    },
+  },
+};
 
 export default new ApolloServer({ typeDefs, resolvers });
